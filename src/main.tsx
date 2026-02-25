5d3ae1a4fe740e9c0b8bced19eebb275517d54eb
Everlegends <yurvopros@gmail.com>
2026-02-25 23:55:39 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const p = params.get("p");
+if (p) {
+  params.delete("p");
+  const rest = params.toString();
+  window.history.replaceState({}, "", p + (rest ? `?${rest}` : ""));
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
