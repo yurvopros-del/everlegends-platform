9359d7984ba24b4d651ee9f839daf1344f8fdf16
Everlegends <yurvopros@gmail.com>
2026-02-25 23:22:53 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,45 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { LanguageProvider } from "@/hooks/useLanguage";
+
 import Index from "./pages/Index";
 import CookiePolicy from "./pages/CookiePolicy";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-import CookieBanner from "./components/CookieBanner";
 import NotFound from "./pages/NotFound";
+
+import CookieBanner from "./components/CookieBanner";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
+const App = () => {
+  // Vite sets this to "/everlegends-platform/" in production because of vite.config.ts base.
+  const basename = import.meta.env.BASE_URL;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
         <LanguageProvider>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/ru" element={<Index />} />
-            <Route path="/cookie-policy" element={<CookiePolicy />} />
-            <Route path="/ru/cookie-policy" element={<CookiePolicy />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/ru/privacy-policy" element={<PrivacyPolicy />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <CookieBanner />
+          <BrowserRouter basename={basename}>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/cookie-policy" element={<CookiePolicy />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+
+            <CookieBanner />
+
+            <Toaster />
+            <Sonner />
+          </BrowserRouter>
         </LanguageProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
